33a42391a449f921cf9923cd6a59b326ddd6f948
ashok <ashok@gmail.com>
2023-05-15 09:41:25 +0530

    first commit

---
+++ Online_Pizza_Ordering_System/Online_Pizza_Ordering_System/Repositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Online_Pizza_Ordering_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Pizza_Ordering_System.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task UpdateCardAsync(CardPayment cardpayment);
+        Task CreateCardAsync(CardPayment cardpayment);
+
+    }
+}
